c02e36e6db11417edccaf8de3bbf2fa07f876c0b
guilherme <guihm.dev@gmail.com>
2022-11-06 22:12:01 -0300

    minor changes

--- tvm/pages/index.js
+++ tvm/pages/index.js
@@ -1,25 +1,12 @@
 import Head from "next/head"
 
 
-export default function Home() {
+const Home = () => {
   return (
     <div>
-      <Head>
-        <title>TVM</title>
-        <meta name="description" content="Whatever" />
-      </Head>
-
-      <main>
-        <h1>Welcome to TVM</h1>
-
-        <p>
-          Movies and TV Shows
-        </p>
-
-      </main>
-
-      <footer>
-      </footer>
     </div>
   )
 }
+
+
+export default Home
