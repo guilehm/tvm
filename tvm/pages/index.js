980f890c368d0b3a79c0474cf955e715532da516
guilherme <guihm.dev@gmail.com>
2022-11-06 22:01:20 -0300

    create main files

---
+++ tvm/pages/index.js
@@ -0,0 +1,25 @@
+import Head from "next/head"
+
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>TVM</title>
+        <meta name="description" content="Whatever" />
+      </Head>
+
+      <main>
+        <h1>Welcome to TVM</h1>
+
+        <p>
+          Movies and TV Shows
+        </p>
+
+      </main>
+
+      <footer>
+      </footer>
+    </div>
+  )
+}
