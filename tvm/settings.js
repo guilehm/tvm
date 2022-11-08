24075c384e20e526a7406cba60384b35bbb6473c
guilherme <guihm.dev@gmail.com>
2022-11-07 22:19:03 -0300

    update settings

--- tvm/settings.js
+++ tvm/settings.js
@@ -1,3 +1,3 @@
 export const API_URL = "https://api.themoviedb.org/3/"
-export const API_ACCESS_TOKEN = process.env.NEXT_PUBLIC_API_ACCESS_TOKEN
+export const API_ACCESS_TOKEN = process.env.API_ACCESS_TOKEN
 export const BASE_SITE_URL = "https://www.themoviedb.org/"
