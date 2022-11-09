6e9e7161826341cdbc5a1072c6abef395e539c76
guilherme <guihm.dev@gmail.com>
2022-11-09 20:59:03 -0300

    create make request function

--- tvm/src/services/api-service.js
+++ tvm/src/services/api-service.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-import { BASE_PROXY_URL, BASE_SITE_URL } from "../../settings"
+import { BASE_PROXY_URL, BASE_SITE_URL, API_ACCESS_TOKEN } from "../../settings"
 
 
 const client = axios.create({ baseURL: BASE_PROXY_URL })
@@ -14,7 +14,12 @@ export function makeUrl(endpoint, query = "", page = 1) {
   const params = new URLSearchParams({ query, page })
   return `${endpoint}?${params}`
 }
-
-export function getImageUrl(path) {
-  return `${BASE_SITE_URL}t/p/w300${path}`
+export async function makeRequest(url, ...params) {
+  return await fetch(url, {
+    headers: {
+      "ContentType": "application/json",
+      "Authorization": `Bearer ${API_ACCESS_TOKEN} `
+    },
+    ...params,
+  })
 }
