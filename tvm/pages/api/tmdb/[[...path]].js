ad38f872b7ded56108fbb8d218d94624f1334f8f
guilherme <guihm.dev@gmail.com>
2022-11-09 19:39:30 -0300

    refactor proxy

--- tvm/pages/api/tmdb/[[...path]].js
+++ tvm/pages/api/tmdb/[[...path]].js
@@ -1,17 +1,13 @@
 import { API_URL, API_ACCESS_TOKEN } from "../../../settings"
 
 export default async function handler(req, res) {
-  const url = req.url.slice(9)
-  try {
-    const response = await fetch(`${API_URL}${url}`, {
-      headers: {
-        "ContentType": "application/json",
-        "Authorization": `Bearer ${API_ACCESS_TOKEN} `
-      },
-    })
-    const data = await response.json()
-    return res.status(200).json(data)
-  } catch {
-    return res.status(500)
-  }
+  const response = await fetch(`${API_URL}${req.url.slice(9)}`, {
+    headers: {
+      "ContentType": "application/json",
+      "Authorization": `Bearer ${API_ACCESS_TOKEN} `
+    },
+  })
+  if (response.status !== 200) return res.status(response.status).end(response.statusText)
+  const data = await response.json()
+  return res.status(response.status).json(data)
 }
