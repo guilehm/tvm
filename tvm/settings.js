69b4b62d2cc34267612e521a3d554bf0fa0e4532
guilherme <guihm.dev@gmail.com>
2022-11-08 23:06:46 -0300

    add base proxy url

--- tvm/settings.js
+++ tvm/settings.js
@@ -1,3 +1,4 @@
 export const API_URL = "https://api.themoviedb.org/3/"
 export const API_ACCESS_TOKEN = process.env.API_ACCESS_TOKEN
 export const BASE_SITE_URL = "https://www.themoviedb.org/"
+export const BASE_PROXY_URL = "/api/tmdb/"
