41b4353bc9d4e86df1122f51ec6b1b9a43f44848
guilherme <guihm.dev@gmail.com>
2022-11-09 22:24:24 -0300

    create settings config

--- tvm/settings.js
+++ tvm/settings.js
@@ -2,3 +2,31 @@ export const API_URL = "https://api.themoviedb.org/3/"
 export const API_ACCESS_TOKEN = process.env.API_ACCESS_TOKEN
 export const BASE_SITE_URL = "https://www.themoviedb.org/"
 export const BASE_PROXY_URL = "/api/tmdb/"
+
+export const settings = {
+  siteName: "TVM",
+  siteDescription: "Movies and TV Shows",
+  contact: {
+    name: "Guilherme Luis",
+    email: "guihm.dev@gmail.com",
+    github: "https://github.com/guilehm",
+  },
+  menu: [
+    {
+      title: "movies",
+      url: "",
+    },
+    {
+      title: "tv shows",
+      url: "",
+    },
+    {
+      title: "login",
+      url: "",
+    }
+  ],
+  footer: {
+    about: "About TVM",
+    description: "Some description here"
+  }
+}
