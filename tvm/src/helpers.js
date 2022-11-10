025435adf059c6d114319697f9be8d7ba7575d31
guilherme <guihm.dev@gmail.com>
2022-11-09 23:23:04 -0300

    lint fixes

--- tvm/src/helpers.js
+++ tvm/src/helpers.js
@@ -1,17 +1,17 @@
-import { BASE_SITE_URL } from "../settings";
+import { BASE_SITE_URL } from "../settings"
 
 
 export function slugify(str) {
-  str = str.replace(/^\s+|\s+$/g, '');
-  str = str.toLowerCase();
-  var from = "ÁÄÂÀÃÅČÇĆĎÉĚËÈÊẼĔȆÍÌÎÏŇÑÓÖÒÔÕØŘŔŠŤÚŮÜÙÛÝŸŽáäâàãåčçćďéěëèêẽĕȇíìîïňñóöòôõøðřŕšťúůüùûýÿžþÞĐđßÆa·/_,:;";
-  var to = "AAAAAACCCDEEEEEEEEIIIINNOOOOOORRSTUUUUUYYZaaaaaacccdeeeeeeeeiiiinnooooooorrstuuuuuyyzbBDdBAa------";
+  str = str.replace(/^\s+|\s+$/g, "")
+  str = str.toLowerCase()
+  var from = "ÁÄÂÀÃÅČÇĆĎÉĚËÈÊẼĔȆÍÌÎÏŇÑÓÖÒÔÕØŘŔŠŤÚŮÜÙÛÝŸŽáäâàãåčçćďéěëèêẽĕȇíìîïňñóöòôõøðřŕšťúůüùûýÿžþÞĐđßÆa·/_,:;"
+  var to = "AAAAAACCCDEEEEEEEEIIIINNOOOOOORRSTUUUUUYYZaaaaaacccdeeeeeeeeiiiinnooooooorrstuuuuuyyzbBDdBAa------"
   for (var i = 0, l = from.length; i < l; i++) {
-    str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+    str = str.replace(new RegExp(from.charAt(i), "g"), to.charAt(i))
   }
-  return str.replace(/[^a-z0-9 -]/g, '')
-    .replace(/\s+/g, '-')
-    .replace(/-+/g, '-');
+  return str.replace(/[^a-z0-9 -]/g, "")
+    .replace(/\s+/g, "-")
+    .replace(/-+/g, "-")
 }
 
 export function getImageUrl(path) {
@@ -19,6 +19,6 @@ export function getImageUrl(path) {
 }
 
 export function getMovieDetailUrl(id, title) {
-  return `movies/${id}/${slugify(title)}/`
+  return `movies/${id}/${slugify(title) || "-"}/`
 }
 
