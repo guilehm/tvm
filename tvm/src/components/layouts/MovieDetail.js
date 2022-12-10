f5f644e6342699fc09f0f7e0d483e81a5dd5cc74
gui <guihm.dev@gmail.com>
2022-12-09 22:05:09 -0300

    add backdrop path to movie detail page

--- tvm/src/components/layouts/MovieDetail.js
+++ tvm/src/components/layouts/MovieDetail.js
@@ -1,9 +1,18 @@
 "use client"
 
+import { getImageUrl } from "../../helpers"
+import ImageContainer from "../commons/ImageContainer"
+
 export default function MovieDetail({ data }) {
   return (
     <div>
-      Hello from MovieDetail {JSON.stringify(data)}
-    </div>
+      Hello from MovieDetail
+      <p>Original Title: {data.original_title}</p>
+      {data.poster_path &&
+        <ImageContainer
+          src={getImageUrl(data.poster_path)}
+          alt={data.original_title}
+          fill />}
+    </div >
   )
 }
